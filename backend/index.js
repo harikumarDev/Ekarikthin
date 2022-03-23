1a780a00eb07eb3d66d3aa6a67fd83b7b0f146cc
Hari kumar <hari70319@gmail.com>
2022-03-23 13:03:43 +0530

    Deploy on heroku

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 const connectDb = require("./config/db");
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 process.on("uncaughtException", (err) => {
   console.log(`ERROR: ${err.stack}`);
